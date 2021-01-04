1494b4f9864b844045593d06b78d9b22c5bdf5e6
Zoe Chirico <56097500+ZoeChiri@users.noreply.github.com>
2021-01-04 15:30:09 -0500

    Add files via upload

---
+++ day0.go
@@ -0,0 +1,12 @@
+package day0
+
+func Day0(){
+	TYPE string
+	M map[string]interface{}
+}
+
+func NewDay0() *Day0{
+	d := &Day0{}
+	d.TYPE="Cat"
+	return d
+}
---
+++ day0_test.go
@@ -0,0 +1,13 @@
+package day0
+
+import (
+	"fmt"
+	"testing"
+	"strings"
+)
+
+func Test (t *testing.T){
+	d := NewDay0()
+
+	print(d.TYPE)
+}
